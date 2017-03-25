dfa3a77fc03b5b71375713b2ee5cbca36a44df0d
Ryan Nowak <nowakra@gmail.com>
2017-03-24 17:21:09 -0700

    Remove WebListener and unused auth stuff

--- src/MusicStore/Program.cs
+++ src/MusicStore/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Net.Http.Server;
 
 namespace MusicStore
 {
@@ -28,32 +26,8 @@ namespace MusicStore
                 .UseConfiguration(config)
                 .UseIISIntegration()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup("MusicStore");
-
-            if (string.Equals(builder.GetSetting("server"), "Microsoft.AspNetCore.Server.WebListener", System.StringComparison.Ordinal))
-            {
-                var environment = builder.GetSetting("environment") ??
-                    Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-                if (string.Equals(environment, "NtlmAuthentication", System.StringComparison.Ordinal))
-                {
-                    // Set up NTLM authentication for WebListener like below.
-                    // For IIS and IISExpress: Use inetmgr to setup NTLM authentication on the application vDir or
-                    // modify the applicationHost.config to enable NTLM.
-                    builder.UseWebListener(options =>
-                    {
-                        options.Listener.AuthenticationManager.AuthenticationSchemes = AuthenticationSchemes.NTLM;
-                    });
-                }
-                else
-                {
-                    builder.UseWebListener();
-                }
-            }
-            else
-            {
-                builder.UseKestrel();
-            }
+                .UseStartup("MusicStore")
+                .UseKestrel();
 
             var host = builder.Build();
 
