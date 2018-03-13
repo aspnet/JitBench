a4c2be8e37ebcd5e2cc4083d349e47439aa863af
Adam Sitnik <adam.sitnik@microsoft.com>
2018-03-13 20:58:23 +0100

    copy the Program.cs and Startup.cs files

---
+++ src/AllReady/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace AllReady
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, config) => config.SetBasePath(ctx.HostingEnvironment.ContentRootPath).AddJsonFile("version.json"))
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
