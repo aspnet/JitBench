8097e650a4ede50e0d335084171914b5339a6ddb
Adam Sitnik <adam.sitnik@microsoft.com>
2018-03-13 21:37:08 +0100

    hardcode the hosting url

--- src/AllReady/Program.cs
+++ src/AllReady/Program.cs
@@ -9,6 +9,8 @@ namespace AllReady
 {
     public class Program
     {
+        const string Url = "http://localhost:5000";
+
         public static void Main(string[] args)
         {
             var jitBench = JitBenchHelper.Start();
@@ -22,8 +24,7 @@ namespace AllReady
 
             jitBench.LogStartup();
 
-            jitBench.PerformHttpRequests("http://localhost:5000", args, 
-                threshholds: new int[] { 100, 250, 500, 750, 1000, 1500, 2000 });
+            jitBench.PerformHttpRequests(Url, args, threshholds: new int[] { 100, 250, 500, 750, 1000, 1500, 2000 });
 
             jitBench.VerifyLibraryLocation();
         }
@@ -33,6 +34,7 @@ namespace AllReady
                 .UseConfiguration(configRoot)
                 .ConfigureAppConfiguration((ctx, config) => config.SetBasePath(ctx.HostingEnvironment.ContentRootPath).AddJsonFile("version.json"))
                 .UseStartup<Startup>()
+                .UseUrls(Url)
                 .ConfigureLogging(factory =>
                 {
                     factory.AddConsole();
